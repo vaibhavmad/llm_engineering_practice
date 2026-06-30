41780d9fe46bf3b326524279fcfdf71a4ed8e8a8
Vaibhav Madaan <dev.vaibhav.88@gmail.com>
2026-06-30 09:22:44 +0530

    Complete day 3

---
+++ day03/day03_main.py
@@ -0,0 +1,52 @@
+"""
+Today we cover:
+1. Base, Chat and Reasoning Models: Uderstanding LLM Types
+2. Frontier Models: GPT, Claude, Gemini & Their Strengths and Pitfalls
+3. Testing ChatGPT-5 and Frontier LLMs Through the Web UI
+4. Testing Claude, Gemini, Grok & DeepSeek with ChatGPT Deep Research
+5. Agentic AI in Action: Deep Research, Claude Code, and Agent Mode
+6. Frontier Models Showdown: Building an LLM Competition Game
+"""
+
+"""
+1. Base, Chat and Reasoning Models: Uderstanding LLM Types
+
+LLMs come in 3 flavors:
+1. Base: LLM just takes the sequence of the info and predicts what comes next. This is like the one we have in our phones keypad. Like if we have predictive messaging on, then on type one word, we get the next predictions. If we keep adding this, we will be using this type of model. Base models are better for fine tuning to learn a new skill. When we want a model to behave in a particular manner by using data set that we provide it with, we should use base models.
+
+2. Chat/Instruct: This is a variant of the base LLM, where base models were trained on large datasets and these models then started responding in the form of chat models as we see today. These are great for content generation and creative tasks (which is a form of content generation). These models are faster and cheaper when compared with reasoning models.
+
+3. Reasoning Model: The chat models were asked to thinking through, or asked to work in steps. These models think and then give output of the response. They decide how much to reason on the basis of the prompt that we gave it. These are great for problem solving. These are kind of the most expensive models, as they also spend tokens while thinking. 
+"""
+
+"""
+2. Frontier Models: GPT, Claude, Gemini & Their Strengths and Pitfalls
+
+Below are some of the frontier models:
+    1. OpenAI, Models: GPT, Chat: ChatGPT
+    2. Anthropic, Models: Claude, Chat: Claude
+    3. Google, Models: Gemini, Chat: Gemini
+    4. X.ai, Models: Grok, Chat: Grok
+    5. Deepseek: Models: Deepseek, Chat: Deepseek
+
+These models are great at:
+    1. Synthesizing information
+    2. Content creation
+    3. Coding
+
+They also have some limitations:
+    1. They do not have expertise in all domains
+    2. Limited access to latest information due to cut off dates. This however has been mitigated by adding the capability of searching the latest information in most of the agents or rather interfaces now.
+    3. LLMs can be very confident and still make mistakes. This is improving though. Therefore, it is important to review each and every response for accuracy, before using that information. Therefore, we can conclude that LLMs perform best under supervision.
+"""
+
+"""
+3. Testing ChatGPT-5 and Frontier LLMs Through the Web UI
+4. Testing Claude, Gemini, Grok & DeepSeek with ChatGPT Deep Research
+5. Agentic AI in Action: Deep Research, Claude Code, and Agent Mode
+6. Frontier Models Showdown: Building an LLM Competition Game
+Asked some questions to chatgpt, including some trick questions.
+Asked some questions to other LLM chat engines, including some trick questions.
+Also saw demos of few features of chat interfaces, such as deep research, browser within chatgpt, image generation, agent mode & coding.
+Outsmart game developed by ed, saw how it runs etc. Today was more about understanding of what LLMs can do and what they are not good at. 
+"""

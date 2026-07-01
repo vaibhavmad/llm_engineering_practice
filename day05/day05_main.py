c81fdd840b831c2924e5c01a5ee76072e40296f1
Vaibhav Madaan <dev.vaibhav.88@gmail.com>
2026-07-01 09:23:39 +0530

    Initiate Day05

---
+++ day05/day05_main.py
@@ -0,0 +1,8 @@
+"""
+Covered today:
+    1. Building a Sales Brochure Generator with OpenAI Chat Completions API
+    2. Building JSON Prompts and Using OpenAI's Chat Completions API
+    3. Chaining GPT Calls: Building an AI Company Brochure Generator
+    4. Building a Brochure Generator with GPT-4 and Streaming Results
+    5. Business Applications, Challenges & Building Your AI Tutor
+"""

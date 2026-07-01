58ebed6c536bcf951f50813092a204fab6d403d1
Vaibhav Madaan <dev.vaibhav.88@gmail.com>
2026-07-01 14:08:53 +0530

    Task1 Details Added, new file for task1 (company_sales_brochure.py) created

---
+++ day05/company_sales_brochure.py
@@ -0,0 +1,15 @@
+"""
+Task: Company sales brochure generator
+    - Create a product that can generate marketing brochures about a company
+        1. For prospective clients
+        2. For investors
+        3. For recruitment
+    
+    - The technology
+        1. Use OpenAI API
+        2. Use one-shot prompting
+        3. Stream back results and show with formatting
+
+    - Input
+        1. We shall be provided a company name and their primary website
+"""
--- day05/day05_main.py
+++ day05/day05_main.py
@@ -6,3 +6,20 @@ Covered today:
     4. Building a Brochure Generator with GPT-4 and Streaming Results
     5. Business Applications, Challenges & Building Your AI Tutor
 """
+
+"""
+Task 1: Company sales brochure generator
+    - Create a product that can generate marketing brochures about a company
+        1. For prospective clients
+        2. For investors
+        3. For recruitment
+    
+    - The technology
+        1. Use OpenAI API
+        2. Use one-shot prompting
+        3. Stream back results and show with formatting
+
+    - Input
+        1. We shall be provided a company name and their primary website
+"""
+
